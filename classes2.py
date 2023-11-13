424e2bee78ee38c071cc0c62dd281d84187680bb
VictorGa2003 <115746295+VictorGa2003@users.noreply.github.com>
2023-11-13 20:17:20 -0300

    Update classes2.py

--- classes2.py
+++ classes2.py
@@ -3,6 +3,11 @@ class Quarto:
         self.numero = numero
         self.tipo = tipo
         self.preco_por_noite = preco_por_noite
+    def calcular_custo_estadia(self, numero_de_noites):
+        return numero_de_noites * self.preco_por_noite
+
+    def __str__(self):
+        return f"Número do Quarto: {self.numero}, Tipo: {self.tipo}, Preço por Noite: {self.preco_por_noite}"
 
 class Reserva:
     def _init_(self, checkin, checkout, cliente, quarto):
@@ -22,7 +27,12 @@ class CheckOut:
     def _init_(self, data_hora, cliente, quarto):
         self.data_hora = data_hora
         self.cliente = cliente
-        self.quarto = quarto
+        self.quarto = quarto   
+    def __str__(self):
+        return f"Data e Hora: {self.data_hora}, Cliente: {self.cliente}, Quarto: {self.quarto}"
+    checkout_instance = CheckOut(data_hora, cliente, quarto)
+    print(checkout_instance)
+
 
 class ComentarioHotel:
     def _init_(self, cliente, data, classificacao, comentario):
@@ -36,6 +46,11 @@ class ServicoHotel:
         self.nome = nome
         self.horarios_funcionamento = horarios_funcionamento
         self.custo_adicional = custo_adicional
+    def calcular_custo_total(self, custo_base):
+        return custo_base + self.custo_adicional
+
+    def __str__(self):
+        return f"Nome do Serviço: {self.nome}, Horários de Funcionamento: {self.horarios_funcionamento}, Custo Adicional: {self.custo_adicional}"
 
 class EventoHotel:
     def _init_(self, nome, data, localizacao, detalhes):
